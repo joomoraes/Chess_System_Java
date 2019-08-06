b3c58bcf4ef8ea1c78ac90da0969e741a029facd
Joao Pedro <vyrkshowe@gmail.com>
2019-08-06 03:43:11 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,6 +8,9 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
+	
+	//https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -27,6 +30,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// http://stackoverflow.com/questions/2979383/java-clear-the-console	
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.println();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 		String s = sc.nextLine();
@@ -35,7 +44,7 @@ public class UI {
 		return new ChessPosition(column, row);
 		}
 		catch(RuntimeException e) {
-			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8. ");
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from A1 to H8. ");
 		}
 	}
 	
